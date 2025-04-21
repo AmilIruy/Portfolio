415b7faf891b9ef7ae2d44e77b4645eab2aaaf72
TheFatCat0 <yuriaciole@hotmail.com>
2025-04-21 19:25:54 -0300

    CSS patch

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -2,20 +2,25 @@ import React, { useMemo } from 'react';
 
 const Bubbles = () => {
   const bubbles = useMemo(() => {
-    return Array.from({ length: 25 }).map((_, index) => ({
-      id: index,
-      animationDelay: Math.random() * 40
-    }));
+    return Array.from({ length: 25 }).map((_, index) => {
+      const i = Math.floor(Math.random() * 10 + 1); 
+      const delay = Math.random() * 40;
+      return {
+        id: index,
+        i,
+        animationDelay: -delay.toFixed(2),
+      };
+    });
   }, []);
 
   return (
     <div className="bolhas">
       {bubbles.map((bubble) => (
-        <span 
-          key={bubble.id} 
-          style={{ 
-            '--i': bubble.id,
-            animationDuration: `${120 / bubble.animationDelay}s`
+        <span
+          key={bubble.id}
+          style={{
+            '--i': bubble.i,
+            animationDelay: `${bubble.animationDelay}s`,
           }}
         ></span>
       ))}
